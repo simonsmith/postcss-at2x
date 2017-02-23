82d4f5dcc12fdfdcc19db33934078ae96ba9917d
Simon Smith <simonsmith@users.noreply.github.com>
2017-02-23 17:18:09 +0000

    Switch to Jest
    Allows much easier comparison with CSS output

--- test/test.js
+++ test/test.js
@@ -1,5 +1,4 @@
 import postcss from 'postcss';
-import {expect} from 'chai';
 import fs from 'fs';
 import path from 'path';
 import plugin from '../lib/';
@@ -8,35 +7,34 @@ function readFixture(filename) {
   return fs.readFileSync(path.join('test/fixtures', filename), 'utf-8');
 }
 
-function test(input, output, opts, done) {
+function runTest(input, opts, done) {
   input = readFixture(`${input}`);
-  output = readFixture(`${output}`);
 
   postcss([plugin(opts)]).process(input).then((result) => {
-    expect(result.css).to.eql(output);
-    expect(result.warnings()).to.be.empty;
+    expect(result.css).toMatchSnapshot();
+    expect(result.warnings()).toBeEmpty();
     done();
   }).catch(done);
 }
 
 describe('postcss-at2x', () => {
   it('should add device-pixel-ratio rules', (done) => {
-    test('at2x.css', 'at2x.out.css', {}, done);
+    runTest('at2x.css', {}, done);
   });
 
   it('should allow retina image scoped to media query', (done) => {
-    test('scoped.css', 'scoped.out.css', {}, done);
+    runTest('scoped.css', {}, done);
   });
 
   it('should allow a custom identifier for retina file names', (done) => {
-    test('identifier.css', 'identifier.out.css', {identifier: '-retina'}, done);
+    runTest('identifier.css', {identifier: '-retina'}, done);
   });
 
   it('should allow mulitple background images to be replaced', (done) => {
-    test('multibg.css', 'multibg.out.css', {}, done);
+    runTest('multibg.css', {}, done);
   });
 
   it('should process image and add background size', (done) => {
-    test('read-background-size.css', 'read-background-size.out.css', {detectImageSize: true}, done);
+    runTest('read-background-size.css', {detectImageSize: true}, done);
   });
 });
